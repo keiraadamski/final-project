51dd5a7814aeff07c500451cad1deb1770fb08f3
keiraadamski <149716312+keiraadamski@users.noreply.github.com>
2023-11-07 22:07:34 -0500

    Add files via upload

---
+++ preposal.md.py
@@ -0,0 +1,13 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+I have an idea to do a punnett square where the user will input alleles based on the eye and hair color of two individuals or parents. The output should include all the possible outcomes of alleles that those parents could have as offspring.
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+I plan to do my final project individually.
+
+## Do you have any questions of your own?
+
+I do not have any questions at the moment.
