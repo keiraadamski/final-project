5817ca4a1260b6b44020081e848f8667d84da759
keiraadamski <149716312+keiraadamski@users.noreply.github.com>
2023-11-27 17:37:54 -0500

    Create status.py

---
+++ status.py
@@ -0,0 +1,21 @@
+# Status Report
+
+#### Your name.
+Keira Adamski
+
+#### Your section leader's name
+Yang Zhao
+
+#### Project title
+"Testing the Stroop Effect"
+
+Short answers for the below questions suffice. If you want to alter your planfor your project (and obtain approval for the same), be sure to email your section leader directly!
+
+#### What have you done for your project so far?
+I was able to install pygame and started coding the the word of a color to come up in a different color using random.choice
+
+#### What have you not done for your project yet?
+I have to make a timer that ends the program after sixty seconds and buttons for the options of what color the word displayed is.
+
+#### What problems, if any, have you encountered?
+It was a bit difficult to install pygame onto my Mac since it is an older verison and I had to update it, but I was able to figure it out with research.
